43d6e20d153eebc3d93b1d830653fa7d8bb5c20f
Michael McDonald <m_J_mcdonald@berkeley.edu>
2018-06-28 15:15:31 -0700

    Fixed small bug

--- test_render.py
+++ test_render.py
@@ -3,7 +3,7 @@ from internal_state.simulator_state import *
 from driving_gui.gui import *
 
 sim = SimulatorState(30, 100, 80)
-sim.add_road(Road(50, 50, 100, 0))
+sim.add_road(Road(0, 50, 50, 100, 0))
 
 g = GUI(sim)
 g.draw_timestep(0)
